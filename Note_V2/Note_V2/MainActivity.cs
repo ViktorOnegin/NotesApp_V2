ae0de2cabac16e19a10535077b732bf40af86ebc
ViktorOnegin <33310165+ViktorOnegin@users.noreply.github.com>
2019-01-22 12:46:17 +0200

    toolbar added

---
+++ Note_V2/Note_V2/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Views;
+using Note_V2;
+using Android.Content;
+
+namespace ToolbarExample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetActionBar(toolbar);
+            ActionBar.Title = "My toolbar";
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.top_menus, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.menu_add:
+                    {
+                        var startActivity = new Intent(this, typeof(addNotesActivity));
+                        StartActivity(startActivity);
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
